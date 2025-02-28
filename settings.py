4c7cf072263d47fe70c45345ecab1b625148c64e
cameron MacDonald <g8mej@gc112m44.cs.unb.ca>
2025-02-28 12:04:29 -0400

    Syncing local with remote

---
+++ settings.py
@@ -0,0 +1,13 @@
+#/usr/bin/env python
+APP_HOST = 'cs3103.cs.unb.ca'
+APP_PORT =  8013
+APP_DEBUG = True
+
+MYSQL_HOST = 'localhost'
+MYSQL_USER = 'g8mej'
+MYSQL_PASSWD = 'd8FttT4M'
+MYSQL_DB = 'g8mej'
+
+SECRET_KEY = 'd41d8cd98f00b204e9800998ecf8427e'
+
+LDAP_HOST =  'ldap-student.cs.unb.ca'
