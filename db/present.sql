f8abacb6fa0db26965c87dbb989a4d5f7aa5188a
EmilioCarretero <129095223+EmilioCarretero@users.noreply.github.com>
2025-03-26 16:58:02 -0300

    Update present.sql
    Added procedures

--- db/present.sql
+++ db/present.sql
@@ -12,29 +12,79 @@ CREATE TABLE Presents (
 
 
 DELIMITER $$
-
 CREATE PROCEDURE getPresentsByListID(IN listID INT)
 BEGIN
     SELECT presentID, presentName, description, status, priority
     FROM Presents
     WHERE presentListID = listID;
 END $$
-
 DELIMITER ;
 
+DELIMITER $$
+CREATE PROCEDURE getPresents()
+BEGIN
+    SELECT * FROM Presents;
+END $$
+DELIMITER ;
 
+DELIMITER $$
+CREATE PROCEDURE getPresentByID(IN presentIDIn INT)
+BEGIN
+    SELECT * FROM Presents WHERE presentID = presentIDIn;
+END $$
+DELIMITER ;
 
 DELIMITER $$
 CREATE PROCEDURE addPresent(
     IN listID INT, 
     IN pName VARCHAR(255), 
-    IN pDescription VARCHAR(511), 
-    IN pStatus TINYINT, 
-    IN pPriority TINYINT
+    IN descriptionIn VARCHAR(511), 
+    IN statusIn TINYINT, 
+    IN priorityIn TINYINT
 )
 BEGIN
     INSERT INTO Presents (presentListID, presentName, description, status, priority)
-    VALUES (listID, pName, pDescription, pStatus, pPriority);
+    VALUES (listID, pName, descriptionIn, statusIn, priorityIn);
+END $$
+DELIMITER ;
+
+DELIMITER //
+DROP PROCEDURE IF EXISTS deletePresent //
+CREATE PROCEDURE deletePresent(IN presentIDIn INT)
+BEGIN
+    DELETE FROM Presents WHERE presentID = presentIDIn;
+END //
+DELIMITER ;
+
+DELIMITER $$
+CREATE PROCEDURE updatePresent(IN presentNameIn VARCHAR(255), IN descriptionIn TEXT, IN statusIn BOOLEAN, IN priorityIn INT, IN presentIDIn INT)
+BEGIN
+    UPDATE Presents
+    SET presentName = presentNameIn,
+        description = descriptionIn,
+        status = statusIn,
+        priority = priorityIn
+    WHERE presentID = presentIDIn;
+END $$
+DELIMITER ;
+
+DELIMITER $$
+DROP PROCEDURE IF EXISTS searchPresents $$
+CREATE PROCEDURE searchPresents(
+    IN presentNameIn VARCHAR(255), 
+    IN descriptionIn TEXT, 
+    IN statusIn BOOLEAN, 
+    IN priorityIn INT, 
+    IN presentListIDIn INT
+)
+BEGIN
+    SELECT * FROM Presents
+    WHERE 
+        (presentNameIn IS NULL OR presentName = presentNameIn) AND
+        (descriptionIn IS NULL OR description = descriptionIn) AND
+        (statusIn IS NULL OR status = statusIn) AND
+        (priorityIn IS NULL OR priority = priorityIn) AND
+        (presentListIDIn IS NULL OR presentListID = presentListIDIn);
 END $$
 
 DELIMITER ;
